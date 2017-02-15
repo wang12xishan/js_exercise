15e23ea28634d55e361a1065b07e03006c13c465
wang12xishan <wang19860520+cn@gmail.com>
2017-02-15 11:44:33 +0800

    Initial commit

---
+++ js练习1/myjs.js
@@ -0,0 +1,36 @@
+/**
+ * Created by oukisan on 2017/2/14.
+ */
+ // alert('Hello, world');
+
+
+
+// if (Day ==1) {
+//      alert("星期一");
+// }
+//
+// switch (Day){
+//     case 1:alert("1");break;
+//     case 2:alert("2");break;
+//     case 3:alert("3");break;
+//     case 4:alert("4");break;
+//     case 5:alert("5");break;
+//     case 6:alert("6");break;
+//     case 7:alert("7");break;
+//     default :alert("noting");
+//  }
+
+var eles_P,eles_Div;
+ eles_P = document.getElementsByTagName("p");
+ eles_Div = document.getElementsByTagName("div");
+    for (var i=0;i<eles_P.length;i++){
+        console.log(eles_P[i])
+    }
+
+
+
+ // for (var i in eles_P){
+ //     document.write(i);
+ //     document.write(eles_P.item());
+ // }
+ //
